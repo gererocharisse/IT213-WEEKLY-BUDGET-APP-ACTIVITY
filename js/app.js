dd603bb0325b2a309eb7972b398af2404c563d04
gererocharisse <91642206+gererocharisse@users.noreply.github.com>
2021-10-31 16:38:23 +0800

    Validating the expenses, Read the values from the budget form, Inser into HTML, Clear the error, Adding expenses into the list, Create a li, Create the template, Working with the remaining budget, Substract expense amount from budget, Add some classes and remove others

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@ class Budget {
         this.budget = Number( budget );
         this.budgetLeft = this.budget;
     }
+
+    substractFromBudget(amount) {
+        return this.budgetLeft -= amount;
+    }
 }
 
 
@@ -15,6 +19,50 @@ class HTML {
 
         budgetTotal.innerHTML = `${amount}`;
         budgetLeft.innerHTML = `${amount}`;
+    }
+
+
+    printMessage(message, className) {
+        const messageWrapper = document.createElement('div');
+        messageWrapper.classList.add('text-center', 'alert', className);
+        messageWrapper.appendChild(document.createTextNode(message));
+
+        document.querySelector('.primary').insertBefore(messageWrapper, addExpenseForm);
+
+
+        setTimeout(function() {
+            document.querySelector('.primary .alert').remove();
+            // addExpenseForm.reset();
+        }, 3000);
+
+    }
+
+    addExpenseToList(name, amount) {
+        const expensesList = document.querySelector('#expenses ul');
+
+        const li = document.createElement('li');
+        li.className = "list-group-item d-flex justify-content-between align-items-center";
+
+        li.innerHTML = `
+            ${name}
+            <span class="badge badge-primary badge-pill">$ ${amount}</span>
+        `;
+
+        expensesList.appendChild(li);
+    }
+
+    trackBudget(amount) {
+        const budgetLeftDollars = budget.substractFromBudget(amount);
+        budgetLeft.innerHTML = `${budgetLeftDollars}`;
+
+        if( (budget.budget / 4 ) > budgetLeftDollars ) {
+            budgetLeft.parentElement.parentElement.classList.remove('alert-succes', 'alert-warning');
+            budgetLeft.parentElement.parentElement.classList.add('alert-dander');
+
+        } else if( (budget.budget / 2 ) > budgetLeftDollars ) {
+            budgetLeft.parentElement.parentElement.classList.remove('alert-succes');
+            budgetLeft.parentElement.parentElement.classList.add('alert-warning');
+        }
     }
 }
 
@@ -50,8 +98,23 @@ function eventListeners() {
 
     addExpenseForm.addEventListener('submit', function(e) {
         e.preventDefault();
+
+        
+        const expenseName = document.querySelector('#expense').value;
+        const amount = document.querySelector('#amount').value;
+
+        if(expenseName === '' || amount === '') {
+            html.printMessage('There was an error, all the fields are mandatory', 'alert-danger');
+        } else {
+
+            html.addExpenseToList(expenseName, amount);
+            html.trackBudget(amount);
+            html.printMessage('Added...', 'alert-succes');
+
+        }
     });
 }
+
 
 
 
